c61da78181e7a82e01ae9db3ea36410eabcca3c1
sun <shk99061@gmail.com>
2021-10-27 13:11:59 +0900

    page

---
+++ src/components/templates/Receipt.tsx
@@ -0,0 +1,13 @@
+import { Header } from '../atoms'
+import { Form, End, List } from '../organisms';
+
+export const Receipt = () => {
+  return (
+    <>
+      <Header />
+      <Form />
+      <List />
+      <End />
+    </>
+  );
+}
